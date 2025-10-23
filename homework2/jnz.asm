8b71e4c6fa615f6909f8a8df79c5cf39cb6a4be8
Dr.bin <1436956920@qq.com>
2025-10-23 17:01:47 +0800

    init and finish homework1
    homework2 is half finished

---
+++ homework2/jnz.asm
@@ -0,0 +1,50 @@
+data segment
+    space db ' $'
+    newline db 0dh, 0ah, '$'
+data ends
+
+code segment
+    assume cs:code, ds:data
+start:
+    mov ax, data
+    mov ds, ax
+    
+    mov bl, 'a'
+    mov cx, 26
+    mov dh, 0
+    
+print_loop:
+    ; 打印字符
+    mov dl, bl
+    mov ah, 02h
+    int 21h
+    
+    ; 打印空格
+    mov dx, offset space
+    mov ah, 09h
+    int 21h
+    
+    inc bl
+    inc dh
+    
+    ; 检查是否需要换行
+    cmp dh, 13
+    jne skip_newline    ; 条件跳转：不等于时跳转
+    
+    ; 执行换行
+    mov dx, offset newline
+    mov ah, 09h
+    int 21h
+    mov dh, 0
+    
+skip_newline:
+    ; 用条件跳转替代loop指令
+    dec cx              ; CX减1
+    jnz print_loop      ; 条件跳转：结果非零时跳转
+    
+    ; 程序结束
+    mov ah, 4ch
+    int 21h
+    
+code ends
+end start
---
+++ homework2/loop.asm
@@ -0,0 +1,48 @@
+data segment
+    space db ' '  ; 定义空格字符串，$是DOS字符串结束符
+    newline db 0dh, 0ah, '$'  ; 定义换行符(回车+换行)
+data ends
+
+code segment
+    assume cs:code, ds:data
+start:
+    mov ax, data
+    mov ds, ax          ; 设置数据段寄存器
+    
+    mov bl, 'a'         ; BL寄存器存储当前要打印的字母
+    mov cx, 26          ; CX是循环计数器，总共26个字母
+    mov dh, 0           ; DH是行计数器，记录当前行已打印的字符数
+    
+print_loop:
+    ; 打印当前字符
+    mov dl, bl          ; 将字符从BL移到DL（DOS功能要求）
+    mov ah, 02h         ; DOS功能号02h - 显示字符
+    int 21h             ; 调用DOS中断
+    
+    ; 打印空格
+    mov dx, offset space ; DX指向空格字符串的地址
+    mov ah, 02h         
+    int 21h             ; 调用DOS中断
+    
+    inc bl              ; BL加1，指向下一个字母
+    inc dh              ; 行计数器加1
+    
+    ; 检查是否需要换行（每行13个字符）
+    cmp dh, 13          ; 比较DH是否等于13
+    jne no_newline      ; 如果不等于13，跳过换行
+    
+    ; 换行处理
+    mov dx, offset newline ; DX指向换行符字符串
+    mov ah, 09h         ; DOS功能号09h - 显示字符串
+    int 21h
+    mov dh, 0           ; 重置行计数器为0
+    
+no_newline:
+    loop print_loop     ; CX减1，如果CX≠0则跳转到print_loop
+    
+    ; 程序结束
+    mov ah, 4ch         ; DOS功能号4Ch - 程序退出
+    int 21h
+    
+code ends
+end start
